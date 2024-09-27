2d20fc2049c26a2062bd49617d2f22022a564ea7
Lauanelandim <landim0lauane@hotmail.com>
2024-09-27 19:21:17 -0300

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,1 +1,8 @@
+const velocidade = 100
+const multa = 20%
+const condutor = 0 
+console.log( velocidade * 20 ) / 100
+if(velocidade <= multa){
+    console.log();
+} if(velocidade > multa && condutor <= (multa + velocidade))
 
