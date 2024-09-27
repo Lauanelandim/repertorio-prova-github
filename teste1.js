2dff4f8cd8578b8475eae85d46a905749d11c7c7
Lauanelandim <landim0lauane@hotmail.com>
2024-09-27 18:20:31 -0300

    Update teste1.js

--- teste1.js
+++ teste1.js
@@ -1,5 +1,21 @@
-let ddd = "61";
-usuario = prompt("Digite o ddd");
-
-while(ddd!==61);
-    document.write("DDD não cadastrado");
+let DDD = parseInt();
+switch (DDD) {
+    case 61: console.log("Brasilia");
+        break;
+    case 71: console.log("Salvador");
+        break;
+    case 11: console.log("Sao Paulo");
+        break;
+    case 21: console.log("Rio de Janeiro");
+        break;
+    case 32: console.log("Juiz de Fora");
+        break;
+    case 19: console.log("Campinas");
+        break;
+    case 27: console.log("Vitoria");
+        break;
+    case 31: console.log("Belo Horizonte");
+        break;
+    default: console.log("DDD nao cadastrado");
+        break;
+}
